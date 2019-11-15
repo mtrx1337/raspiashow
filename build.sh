e5091abe55ac414af303995b6d2dc8a751e67ce2
Leonard Lorenz <dev@leonardlorenz.de>
2019-11-15 12:12:23 +0100

    further work on the build script. partitions now get added to the loop dev correctly

--- build.sh
+++ build.sh
@@ -1,55 +1,75 @@
 #!/bin/bash
 
+# Global Vars
+MOUNT_POINT="/mnt/raspiashow"
+
+# clean up mounts, mount directories, etc...
 clean_up() {
-    rm -rf "$MOUNT_POINT"
-    partx -v -d "$LOOP_DEV"
-    LOOP_DEV=$(sudo losetup -f)
-    sudo losetup -d $LOOP_DEV
+    echo "UNMOUNTING PARTITIONS"
+    umount $MOUNT_POINT
+    if [[ $? -ne 0 ]]; then echo "Could not unmount partitions, exiting."; $(clean_up); exit 1; fi
+
+    #rm -rf "$MOUNT_POINT"
+
+    REMAINING_LP_DEV=$(losetup -a | awk '{print $1}' | sed 's/://')
+    echo "REMOVING LOOP DEVICE"
+    if [[ -z "$REMAINING_LP_DEV" ]]
+    then
+        for dev in "$REMAINING_LP_DEV"
+        do losetup -d $dev
+        done
+    fi
 }
 
+# check if at least one commandline parameter was supplied
+if [ $# -eq 0 ]; then echo "No arguments supplied."; exit 1; fi
+
+# check if image file exists and is valid
 IMAGE=$1
-MOUNT_POINT="/mnt/raspiashow"
+test -f $IMAGE
+if [[ $? -ne 0 ]]; then echo "File supplied not valid."; fi
 
 # check for root access
 if [[ "$EUID" -ne 0 ]]; then echo "Please run as root."; exit 1; fi
 
-PARTX_RETURN=$(which partx)
 # make sure important programs are installed
-if ! [[ $? -eq 0 ]]; then echo "Please install partx."; exit 1; fi
+which partx &> /dev/null
+if [[ $? -ne 0 ]]; then echo "Please install partx."; exit 1; fi
+which losetup &> /dev/null
+if [[ $? -ne 0 ]]; then echo "Please install losetup."; exit 1; fi
+
+
 
 echo "MOUNTING IMAGE PARTITIONS TO LOOP DEVICE"
-# mount partitions of debian image
-PX_MOUNT_OUTPUT=$(partx -v -a "$IMAGE")
-# which loop device was used?
-LOOP_DEV=$(echo "$PX_MOUNT_OUTPUT" | grep "Trying to use" | awk '{print $4}' | sed "s/'//g")
-# what partitions exist?
-LOOP_PARTIONS=$(echo "$PX_MOUNT_OUTPUT" | grep "partition #" | awk '{print $1}' | sed 's/://g')
 
-# create mount directory for the partitions
-mkdir $MOUNT_POINT
+
+
+# mount partitions of raspbian image
+LOSETUP_MOUNT_OUTPUT=$(losetup -v -f "$IMAGE")
+# which loop device was used?
+LOOP_DEV=$(losetup -a | tail -n 1 | awk '{print $1}' | sed 's/://')
+# add partitions to loop device
+#partx -v --add $LOOP_DEV
+LP_DEV_PARTITIONS=$(blkid | grep loop | awk '{print $1}' | sed 's/://')
+
+
 
 echo "MOUNTING PARTITIONS"
 
+
+
+# create mount directory for the partitions
+test -d "$MOUNT_POINT" || mkdir "$MOUNT_POINT"
+
 # mount all partitions
-for part in $LOOP_PARTIONS
+for part in $LP_DEV_PARTITIONS
 do
+    echo $part
     mount $part $MOUNT_POINT
-    if ! [[ $? -eq 0 ]]; then echo "Could not mount partitions, exiting."; $(clean_up); exit 1; fi
+    if [[ $? -ne 0 ]]; then echo "Could not mount partitions, exiting."; clean_up; exit 1; fi
 done
 
 ###### do stuff with them ######
 ls /mnt/raspiashow
 
-echo "UNMOUNTING PARTITIONS"
-
-# unmount all partitions
-for part in LOOP_PARTIONS
-do
-    umount $MOUNT_POINT
-    if ! [[ $? -eq 0 ]]; then echo "Could not unmount partitions, exiting."; $(clean_up); exit 1; fi
-done
-
-# delete mount directory again
-
 $(clean_up)
-
