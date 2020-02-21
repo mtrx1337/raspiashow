2fdcc9372e9ff10ba478b64f87fb5151507dbade
Leonard Lorenz <dev@leonardlorenz.de>
2020-02-21 16:19:58 +0100

    Now supports css injection.

--- src/browser-app/src/index.js
+++ src/browser-app/src/index.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 
 function sleep(ms) {
     return new Promise(resolve => {
-        setTimeout(resolve,ms);
+        setTimeout(resolve, ms);
     });
 }
 
@@ -30,14 +30,30 @@ function sleep(ms) {
                 console.log(x);
                 config = JSON.parse(x)['configuration'];
             });
-        if (config !== undefined) {
+
+        if (typeof(config) != "undefined") {
             // download config and navigate to each page specified with the delay specified
+            let pages = await browser.pages();
             for (domain of config['domains']) {
-                let pages = await browser.pages();
-                await pages[0].goto(domain);
-                // TODO read css links or styles from configuration.json
+
+                // navigate to specified domain
+                await pages[0].goto(domain[0]);
+
+                // disable scrollbars
                 await pages[0].addStyleTag({content: '::-webkit-scrollbar {display: none;}'});
-                await sleep(config['switch_time']);
+
+                // optional css injection
+                if (typeof(domain[2]) != "undefined") {
+                    try {
+                        await pages[0].addStyleTag({url: domain[2]});
+                    } catch (e) {
+                        console.log("Couldn't fetch styles: " + domain[2]);
+                        continue;
+                    }
+                }
+
+                // wait n amount of seconds between pages
+                await sleep(domain[1] * 1000);
             }
         }
     }
