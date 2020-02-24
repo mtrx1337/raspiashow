58788606bcdbfd75ee0bff2484ec638c52f99681
Leonard Lorenz <dev@leonardlorenz.de>
2020-02-24 10:38:43 +0100

    fixed try / catch

--- src/index.js
+++ src/index.js
@@ -8,57 +8,53 @@ function sleep(ms) {
 }
 
 (async () => {
-    try {
-        browser = await puppeteer.launch({
-            headless: false,
-            executablePath: '/usr/bin/chromium-browser',
-            defaultViewport: null,
-            ignoreDefaultArgs: [
-                '--enable-automation'
-            ],
-            args: [
-                '--hide-scrollbars',
-                '--no-default-browser-check',
-                '--start-fullscreen',
-                '--disable-notifications'
-            ]
-        });
-        let config = undefined;
-        while (true) {
-            await fetch('{{configurationURL}}')
-                .then(x => { return x.text() })
-                .then(x => {
-                    console.log(x);
-                    config = JSON.parse(x)['configuration'];
-                });
+    browser = await puppeteer.launch({
+        headless: false,
+        executablePath: '/usr/bin/chromium-browser',
+        defaultViewport: null,
+        ignoreDefaultArgs: [
+            '--enable-automation'
+        ],
+        args: [
+            '--hide-scrollbars',
+            '--no-default-browser-check',
+            '--start-fullscreen',
+            '--disable-notifications'
+        ]
+    });
+    let config = undefined;
+    while (true) {
+        await fetch('{{configurationURL}}')
+            .then(x => { return x.text() })
+            .then(x => {
+                console.log(x);
+                config = JSON.parse(x)['configuration'];
+            });
 
-            if (typeof(config) != "undefined") {
-                // download config and navigate to each page specified with the delay specified
-                let pages = await browser.pages();
-                for (domain of config['domains']) {
+        if (typeof(config) != "undefined") {
+            // download config and navigate to each page specified with the delay specified
+            let pages = await browser.pages();
+            for (domain of config['domains']) {
 
-                    // navigate to specified domain
-                    await pages[0].goto(domain[0]);
+                // navigate to specified domain
+                await pages[0].goto(domain[0]);
 
-                    // disable scrollbars
-                    await pages[0].addStyleTag({content: '::-webkit-scrollbar {display: none;}'});
+                // disable scrollbars
+                await pages[0].addStyleTag({content: '::-webkit-scrollbar {display: none;}'});
 
-                    // optional css injection
-                    if (typeof(domain[2]) != "undefined") {
-                        try {
-                            await pages[0].addStyleTag({url: domain[2]});
-                        } catch (e) {
-                            console.log("Couldn't fetch styles: " + domain[2]);
-                            continue;
-                        }
+                // optional css injection
+                if (typeof(domain[2]) != "undefined") {
+                    try {
+                        await pages[0].addStyleTag({url: domain[2]});
+                    } catch (e) {
+                        console.log("Couldn't fetch styles: " + domain[2]);
+                        continue;
                     }
-
-                    // wait n amount of seconds between pages
-                    await sleep(domain[1] * 1000);
                 }
+
+                // wait n amount of seconds between pages
+                await sleep(domain[1] * 1000);
             }
-        } catch (err) {
-            console.error(err.message);
         }
     }
 })();
